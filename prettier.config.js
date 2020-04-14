3cfa10829578b8cb50034b5740e14fa8db9d8830
Felipe Silva <felipe.dacs@gmail.com>
2020-04-13 23:28:24 -0300

    release: add aula 4

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // aspas simples ao invés de aspas duplas
+  trailingComma: 'all', // virgula no ultimo elemento de obj/array
+  arrowParens: 'avoid', // não colocar parêntese em arrow functions com somente um parâmetro
+}
