4978e1e82dcf21c4c5ed11b3f22ad1eceaa80e6f
Felipe Silva <felipe.dacs@gmail.com>
2020-04-14 22:01:49 -0300

    release: add aula 10

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,7 +6,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
-// localhost:3333/appointments
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
