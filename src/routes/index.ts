98e2979a592f320aad17025e82b14e57e14957d1
Felipe Silva <felipe.dacs@gmail.com>
2020-04-18 14:12:40 -0300

    release: add aula 18

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
-// repassa qualquer verbo http (get, post, put etc) da rota appointments
+// repassa qualquer verbo http (get, post, put etc) da
 // para a variável/arquivo appointmentsRouter
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUserExist = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExist) {
+      throw new Error('Email address already used.');
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
