e1f848db5ab5cd575065a0458cd86a392c4cec6b
Felipe Silva <felipe.dacs@gmail.com>
2020-04-18 23:36:38 -0300

    release: add aula 27

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,24 +5,20 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    // { user } deixa explícito nesse arquivo o que está sendo gerado
-    // e retornado ao frontend
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  // { user } deixa explícito nesse arquivo o que está sendo gerado
+  // e retornado ao frontend
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
