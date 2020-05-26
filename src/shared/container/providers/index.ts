021c6985617859be27f62a4d8cd5ebf74045a05e
Felipe Silva <felipe.dacs@gmail.com>
2020-05-25 23:57:09 -0300

    release: add aula 49

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+// import IMailProvider from './MailProvider/models/IMailProvider';
+
 // deve depender do ambiente
 // dev é um, prod outro
 container.registerSingleton<IStorageProvider>(
