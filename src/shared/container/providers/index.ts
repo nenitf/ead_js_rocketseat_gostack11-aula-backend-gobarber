6aebbdcdac0998ee6283a99702e8d00439b33528
Felipe Silva <felipe.dacs@gmail.com>
2020-05-28 00:38:47 -0300

    release: add aula 54

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,6 +6,9 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 // deve depender do ambiente
 // dev é um, prod outro
 
@@ -14,7 +17,12 @@ container.registerSingleton<IStorageProvider>(
   DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
