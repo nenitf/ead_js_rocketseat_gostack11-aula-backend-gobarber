0bb8b7e4d5c4e4c92e06c1cbd674eee1f1dee9fa
Felipe Silva <felipe.dacs@gmail.com>
2020-05-30 12:58:16 -0300

    release: add aula 57

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -6,5 +6,5 @@ export default interface IUserRepository {
   findById(id: string): Promise<User | undefined>;
   findByEmail(id: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
-  save(data: ICreateUserDTO): Promise<ICreateUserDTO>;
+  save(data: ICreateUserDTO): Promise<User>;
 }
