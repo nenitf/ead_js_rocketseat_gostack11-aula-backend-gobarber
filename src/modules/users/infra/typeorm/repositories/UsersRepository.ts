5f98161aa3edfd9b042ef2c2729b5972c75ab378
Felipe Silva <felipe.dacs@gmail.com>
2020-05-30 20:07:53 -0300

    :tv: add aula 59

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Not } from 'typeorm';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProviders from '@modules/users/dtos/IFindAllProviders';
 
 import User from '../entities/User';
 
@@ -23,6 +24,28 @@ class UserRepository implements IUsersRepository {
     });
 
     return user;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProviders): Promise<User[]> {
+    let users: User[];
+
+    if (except_user_id) {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(except_user_id),
+        },
+      });
+    } else {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(except_user_id),
+        },
+      });
+    }
+
+    return users;
   }
 
   public async save(user: User): Promise<User> {
--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,8 +1,10 @@
 import User from '../infra/typeorm/entities/User';
 
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import IFindAllProviders from '../dtos/IFindAllProviders';
 
 export default interface IUserRepository {
+  findAllProviders(data?: IFindAllProviders): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(id: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
