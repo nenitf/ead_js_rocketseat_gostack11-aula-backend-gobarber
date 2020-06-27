39cf73e5569d8e398553816a1859d5b802da3d23
Felipe Silva <felipe.dacs@gmail.com>
2020-06-27 15:43:21 -0300

    :tv: add aula 73
    comando para add arquivo versionado (ormconfig) no gitignore:
    git rm --cached ormconfig.json

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'af7d58549a5a94a7cd3c03c5726df8ff',
+    secret: process.env.APP_SECRET,
     // o ideal é '1d', mas quero manter o mesmo jwt nas próximas 14 semanas
     expiresIn: '98d',
   },
