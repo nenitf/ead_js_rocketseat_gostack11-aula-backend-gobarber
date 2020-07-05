43e6962f7af4d83da5ece17b1e30502af825a053
Felipe Silva <felipe.dacs@gmail.com>
2020-07-05 13:50:12 -0300

    :tv: add aula 82

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: process.env.APP_SECRET,
+    secret: process.env.APP_SECRET || 'default',
     // o ideal é '1d', mas quero manter o mesmo jwt nas próximas 14 semanas
     expiresIn: '98d',
   },
